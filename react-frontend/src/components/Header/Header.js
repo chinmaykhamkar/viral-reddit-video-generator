d09e0824dbda46308b66b37440a4f4a7be106499
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2024-01-27 00:52:29 +0530

    bg color purple

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-        
+        <div className='header-name'>Viral TikTok/Shorts generator</div>
     </div>
   )
 }
