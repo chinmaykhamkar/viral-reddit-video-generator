6d08e035738e230acc37369e279213fd6849ff22
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2024-02-01 01:26:51 +0530

    bg change and video added

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-        <div className='header-name'>Viral <p style={{marginLeft:'0.5rem', marginRight:'0.5rem', color:'darkviolet'}}> TikTok/Shorts </p> generator</div>
+        <div className='header-name'>Viral <p className='tiktok '> TikTok/Shorts </p> generator</div>
     </div>
   )
 }
--- react-frontend/src/components/VideoSection/VideoSection.js
+++ react-frontend/src/components/VideoSection/VideoSection.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './VideoSection.css'
+import videoSrc from '../../assets/viral.mp4'
 const VideoSection = () => {
   return (
-    <div>VideoSection</div>
+    <div className='video-section'>
+      <div className='video-title'>
+        Select <p className='template'>template</p> video
+      </div>
+      <div className='video'>
+        <video src={videoSrc} width={300} type="video/mp4" controls></video>
+      </div>
+    </div>
   )
 }
 
