c0b6b46cee4c85a2b0264ac6bcbe180f019fc0b3
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2024-02-22 00:29:51 -0800

    created video template grid

--- react-frontend/src/components/VideoSection/VideoSection.js
+++ react-frontend/src/components/VideoSection/VideoSection.js
@@ -1,14 +1,183 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VideoSection.css'
+import HoverVideoPlayer from 'react-hover-video-player';
+
 import videoSrc from '../../assets/viral.mp4'
+import imageSrc from '../../assets/viral_still.jpg'
 const VideoSection = () => {
+
   return (
     <div className='video-section'>
       <div className='video-title'>
         Select <p className='template'>template</p> video
       </div>
       <div className='video'>
-        <video src={videoSrc} width={300} type="video/mp4" controls></video>
+        <div className='video-divs'>
+
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+
+        <div className='video-divs'>
+          <HoverVideoPlayer
+            videoSrc={videoSrc}
+            pausedOverlay={
+              <img
+                src={imageSrc}
+                alt=""
+                style={{
+                  // Make the image expand to cover the video's dimensions
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            }
+
+          />
+        </div>
+        {/* <HoverVideoPlayer
+          videoSrc={videoSrc}
+          pausedOverlay={
+            <img
+              src={imageSrc}
+              alt=""
+              style={{
+                // Make the image expand to cover the video's dimensions
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+              }}
+            />
+          }
+
+        /> */}
       </div>
     </div>
   )
